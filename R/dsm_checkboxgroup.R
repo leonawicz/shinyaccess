4679ba02105c040f872d425069e11a0cd601caf6
Matthew Leonawicz <matt_leonawicz@esource.com>
2020-02-13 10:19:53 -0700

    Add utility functions, argument normalization, wire up the select argument.

--- R/dsm_checkboxgroup.R
+++ R/dsm_checkboxgroup.R
@@ -1,12 +1,19 @@
 #' DSM checkbox group input
 #'
-#' @param inputId input ID.
-#' @param label text label.
-#' @param choices selection choices.
-#' @param selected not in use
+#' @param inputId The id of the input object.
+#' @param label The label to set for the input object.
+#' @param choices List of values to show checkboxes for. If elements of the
+#' list are named then that name rather than the value is displayed to the user.
+#' If this argument is provided, then choiceNames and choiceValues must not be
+#' provided, and vice-versa. The values should be strings; other types (such as
+#' logicals and numbers) will be coerced to strings.
+#' @param selected The values that should be initially selected, if any.
 #' @param inline not in use
 #' @param value defaults to \code{choices}.
 #' @param width not in use
+#' @param choiceNames,choiceValues See \code{\link[shiny]{checkboxGroupInput}}.
+#' Vector of names and values, must have same length.
+#' @param session The session object passed to function given to shinyServer.
 #'
 #' @return A list of HTML elements that can be added to a UI definition.
 #' @export
@@ -31,16 +38,15 @@
 #'     column(3,
 #'       actionButton("btn3", "Reset checkbox group 3"),
 #'       dsmCheckboxGroupInput("cb3", "DSM1 - Variables to show:",
-#'                             c("Cylinders", "Transmission", "Gears"),
-#'                             selected = c("cyl", "am"),
-#'                             values = c("cyl", "am", "gear")),
+#'                             c("Cylinders" = "cyl", "Transmission" = "am", "Gears" = "gear"),
+#'                             selected = c("cyl", "am")),
 #'       tableOutput("data3")
 #'     ),
 #'     column(3,
 #'       dsmCheckboxGroupInput("cb4", "DSM2 - Variables to show:",
-#'                             c("Cylinders", "Transmission", "Gears"),
 #'                             selected = c("cyl", "am"),
-#'                             values = c("cyl", "am", "gear")),
+#'                             choiceNames = c("Cylinders", "Transmission", "Gears"),
+#'                             choiceValues = c("cyl", "am", "gear")),
 #'       tableOutput("data4")
 #'     )
 #'   )
@@ -74,11 +80,19 @@
 #'
 #' }
 dsmCheckboxGroupInput <- function(inputId, label, choices = NULL, selected = NULL,
-                                  inline = FALSE, values = choices, width = NULL){
+                                  inline = FALSE, width = NULL, choiceNames = NULL, choiceValues = NULL){
+  args <- normalizeChoicesArgs(choices, choiceNames, choiceValues)
+  selected <- shiny::restoreInput(id = inputId, default = selected)
+  print(selected)
   lab <- tolower(gsub("--+", "-", gsub(":", "", gsub(" |/", "-", label))))
-  name <- paste0(lab, "-", values)
-  x <- paste0("\n  <input type='checkbox' id='", name, "' name='", lab, "' value='", values, "'></input>",
-              "\n  <label for='", name, "'>", choices, "</label>")
+  name <- paste0(lab, "-", args$choiceValues)
+  v <- unlist(args$choiceValues)
+  vname <- unlist(args$choiceNames)
+  x <- sapply(seq_along(v), function(i){
+    paste0("\n  <input type='checkbox' id='", name[i], "' name='", lab, "' value='", v[i], "'",
+              if(v[i] %in% selected) " checked='checked'", "'></input>",
+              "\n  <label for='", name[i], "'>", vname[i], "</label>")
+  })
   x <- paste(x, collapse = "\n  ")
   x <- paste0(x, "\n</fieldset>\n")
   tagList(
