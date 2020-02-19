9340e6d8aa585723f9723a5ecc307d5d73998f97
Melissa Utomo <mutomo@administrators-MacBook-Pro-4.local>
2020-02-19 15:51:09 -0700

    completed setValue arg

--- inst/resources/input-binding-sa-checkboxgroup.js
+++ inst/resources/input-binding-sa-checkboxgroup.js
@@ -7,12 +7,23 @@ $.extend(saCheckboxGroupBinding, {
   getValue: function(el) {
     // Find all inputs in checkbox groups and create an array
     let inputElements = Array.from($(el).find('input'));
+
+    let options = Array.from($(el).find('li'));
+    console.log(options)
+    //options is an array of objects
+
+    for (var i = 0; i < options.length; i++) {
+      //console.log(options[i].children[0].value)
+      //console.log(options[i].children[1].innerHTML)
+      options[i] = { value:   options[i].children[0].value,
+                   label:   options[i].children[1].innerHTML };
+    }
+    console.log(options);
     // Find input elements that are checked and find their value
     let values = inputElements.filter(inputElement => inputElement.checked === true).map(inputElement => inputElement.value);
-    console.log(values)
     return values;
   },
-  setValue: function(el, value){
+  setValue: function(el, value) {
     // Clear all checkboxes
     $(el).find('input').prop('checked', false);
     // Loop through which checkboxes should be checked based on the value array and update the checkbox
@@ -23,10 +34,35 @@ $.extend(saCheckboxGroupBinding, {
       }
     }
   },
+getState: function(el) {
+
+    //var $objs = $('input:checkbox[name="' + $escape(el.id) + '"]');
+    // Store options in an array of objects, each with with value and label
+    let options = Array.from($(el).find('li'));
+    //options is an array of objects
+
+    for (var i = 0; i < options.length; i++) {
+      options[i] = { value:   options[i].children[0].value,
+                     label:   options[0].children[1].innerHTML };
+    }
+
+    let labelText = $(el).find('legend').text();
+
+    let state = {
+      label: labelText,
+      value: this.getValue(el),
+      options: options
+    };
+
+    console.log('state = ', state);
+
+    return state;
+  },
   updateLabel: function(el, label) {
     $(el).find('legend').text(label);
   },
   receiveMessage: function(el, data) {
+    console.log($(el).find('legend').text())
     // If data.options exist, replace all the options with new ones
     if (data.hasOwnProperty('options')) {
       $(el).find('.sa-options-group').remove();
