0caea518fffc33cab4b089b7aae0d998a5cde34f
Melissa Utomo <mutomo@administrators-MacBook-Pro-4.local>
2020-03-23 16:36:22 -0600

    updated radio button input binding

--- inst/resources/input-binding-sa-radio.js
+++ inst/resources/input-binding-sa-radio.js
@@ -8,17 +8,30 @@ $.extend(saRadioBinding, {
     // Find all inputs in checkbox groups and create an array
     let inputElements = Array.from($(el).find('input'));
     // Find input elements that are checked and find their value
-    let checkedElement = inputElements.filter(inputElement => inputElement.checked === true);
-    return checkedElement.value;
+    let checkedElement = inputElements.filter(inputElement => inputElement.checked === true)[0].value;
+    return checkedElement;
   },
   setValue: function(el, value) {
-
+    console.log("setValue ", el, value);
+    // Loop through which checkboxes should be checked based on the value array and update the checkbox
+    let inputElements = Array.from($(el).find('input'));
+    for(let i = 0; i < inputElements.length; i++){
+      if(inputElements[i].value === value) inputElements[i].checked = true;
+    }
   },
   updateLabel: function(el, label){
     $(el).find('legend').text(label);
   },
   receiveMessage: function(el, data) {
-
+    // If data.options exist, replace all the options with new ones
+    if (data.hasOwnProperty('options')) {
+      $(el).find('.sa-options-group').remove();
+      $(el,"> legend").append(data.options);
+    }
+    // If data.value exists, update what values are checked or not checked
+    if(data.hasOwnProperty('value')) this.setValue(el, data.value);
+    //If data.label exists, update the legend element
+    if(data.hasOwnProperty('label')) this.updateLabel(el, data.label);
   },
   subscribe: function(el, callback) {
     $(el).on('change.saRadioBinding', function(event) {
